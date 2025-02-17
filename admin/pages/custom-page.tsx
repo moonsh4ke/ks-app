91844a07bcfb36905f55df2a2b507de181cee65a
moonsh4ke <mcespedescubillos@gmail.com>
2025-02-17 20:09:21 -0300

    added custom-page

---
+++ admin/pages/custom-page.tsx
@@ -0,0 +1,11 @@
+// admin/pages/custom-page.tsx
+import Link from 'next/link';
+
+export default function CustomPage () {
+    return (
+        <>
+            <h1>This is a custom Admin UI Page</h1>
+            <p>It can be accessed via the route <Link href="/custom-page">/custom-page</Link></p>
+        </>
+    )
+}
